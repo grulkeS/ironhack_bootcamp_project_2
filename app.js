96b4594666ddffdac94cfc972372ebe5e4e59b3e
Stefan Grulke <grulke@mediamarktsaturn.com>
2019-09-29 15:49:17 +0200

    added sesssion and mongo-connect handling to app.js

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
 
+const session    = require("express-session");
+const MongoStore = require("connect-mongo")(session);
 
 mongoose
   .connect('mongodb://localhost/project2', {useNewUrlParser: true})
@@ -25,6 +27,16 @@ const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.
 const app = express();
 
 // Middleware Setup
+
+app.use(session({
+  secret: "basic-auth-secret",
+  cookie: { maxAge: 60000 },
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    ttl: 24 * 60 * 60 // 1 day
+  })
+}));
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -51,6 +63,8 @@ app.locals.title = 'Express - Generated with IronGenerator';
 
 
 
+const auth = require('./routes/auth');
+app.use('/', auth);
 const index = require('./routes/index');
 app.use('/', index);
 
