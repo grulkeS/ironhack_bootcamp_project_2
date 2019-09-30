155a25bdc80f843e0ea0c70e60b23d7b282f487c
stefan grulke <stefan.grulke@gmx.de>
2019-09-30 23:24:04 +0200

    added dynamic path to api query via object+removal of empty properties

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
 const axios = require('axios');
 
 
@@ -19,40 +19,57 @@ router.get('/search', (req, res, next) => {
 /* POST search page */
 router.post('/search', (req, res, next) => {
 
-  let org_entity_id_type = req.body.org_entity_id_type;
-  let org_entity_ids     = req.body.org_entity_ids;
-  let warehouse_glns     = req.body.warehouse_glns; 
-  let storage_locations  = req.body.storage_locations;
-  let stock_types        = req.body.stock_types;
-  let product_mdng_ids   = req.body.product_mdng_ids; 
-  let product_sap_ids    = req.body.product_sap_ids;
-
-  console.log('------------ ', org_entity_ids); // [ '4901', 'M860'] 
-
-axios.get(`${path}?org_entity_id_type=${org_entity_id_type}&org_entity_ids=${org_entity_ids}&storage_locations=${storage_locations}`,
-{auth: {
-  username: 'stock_user',
-  password: 'stock_pass'
-}}
-)
-.then(list => {
-  console.log('######## ', list.data);
-
-list.data.forEach(element => {element.quantity = parseInt(element.quantity)});
-
-
-  res.render('search', {entries: list.data});
-})
-.catch(err => {
-  console.log('Error while requesting the API: ', err);
-})
-
-
-
-
-
-
+ //let org_entity_id_type = req.body.org_entity_id_type;
+ //let org_entity_ids = req.body.org_entity_ids;
+ //let warehouse_glns = req.body.warehouse_glns;
+ //let storage_locations = req.body.storage_locations;
+ //let stock_types = req.body.stock_types;
+ //let product_mdng_ids = req.body.product_mdng_ids;
+ //let product_sap_ids = req.body.product_sap_ids;
+  let queryObject ={};
+  queryObject["org_entity_id_type"] = req.body.org_entity_id_type;
+  queryObject["org_entity_ids"] = req.body.org_entity_ids;
+  queryObject["warehouse_glns"] = req.body.warehouse_glns;
+  queryObject["storage_locations"] = req.body.storage_locations;
+  queryObject["stock_types"] = req.body.stock_types;
+  queryObject["product_mdng_ids"] = req.body.product_mdng_ids;
+  queryObject["product_sap_ids"] = req.body.product_sap_ids;
   
+  for(let prop in queryObject){
+    if(queryObject[prop]===""){
+      delete queryObject[prop];
+    }
+  };
+  console.log(queryObject);
+  console.log('------------ ', queryObject.org_entity_ids); // [ '4901', 'M860'] 
+
+  //?org_entity_id_type=${org_entity_id_type}&org_entity_ids=${org_entity_ids}&storage_locations=${storage_locations}
+  axios.get(`${path}`,
+    {params: queryObject,
+      auth: {
+        username: 'stock_user',
+        password: 'stock_pass'
+      }
+    }
+  )
+    .then(list => {
+      console.log('######## ', list.data);
+
+      list.data.forEach(element => { element.quantity = parseInt(element.quantity) });
+
+
+      res.render('search', { entries: list.data });
+    })
+    .catch(err => {
+      console.log('Error while requesting the API: ', err);
+    })
+
+
+
+
+
+
+
 });
 
 
