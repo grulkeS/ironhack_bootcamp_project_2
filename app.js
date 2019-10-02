004994138ccdd39e844c23460adc0db0f3a04b39
Stefan Grulke <grulke@mediamarktsaturn.com>
2019-10-02 09:35:21 +0200

    session timeout to 10 minutes

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ const app = express();
 
 app.use(session({
   secret: "basic-auth-secret",
-  cookie: { maxAge: 60000 },
+  cookie: { maxAge: 600000 },
   store: new MongoStore({
     mongooseConnection: mongoose.connection,
     ttl: 24 * 60 * 60 // 1 day
